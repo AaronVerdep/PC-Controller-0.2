ed867251bd1db9ac389cb9b1d17cd7fcd167b1b5
AaronVerdep <139834084+AaronVerdep@users.noreply.github.com>
2023-07-18 13:52:23 -0400

    For control your PC.
    Not Dangeours

---
+++ PC Controller-mycomputer/PC Controller 0.2.bat
@@ -0,0 +1,12 @@
+title Console
+@echo off
+echo Welcome to PC Controller, Press any key for start
+pause
+echo NOW - AaronVerdep Created a file with name My File GitHub
+md My File HitHub
+pause 
+echo NOW - AaronVerdep Restart PC
+taskkill /f /im explorer.exe
+start explorer.exe
+pause
+echo NOW - AaronVerdep stop controlling.
